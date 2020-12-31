444b36069d28ce49bad6776bd1aad486f630f4a2
Razor Kenway <razorkenway.98@gmail.com>
2020-12-31 18:31:26 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/RazorKenway/Meta_Data.git
-cd Meta_Data
+git clone https://github.com/RazorKenway/Meta-Data.git
+cd Meta-Data
 echo -e "\e[101m""    Update Was Sucsessfully  ""\e[49m"
 ls
